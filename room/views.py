3dd24b75515b12f5629b7ef6aaee41761448a3e6
Brzydal <damianfilipkowski@gmail.com>
2017-03-18 21:27:57 +0100

    View to Add New Reservation Added

--- room/views.py
+++ room/views.py
@@ -48,18 +48,44 @@ def room_details(request,room_id):
     return render(request,'room/room_details.html',context)
 
 class AddReservation(View):
+    error = None
     
     def get(self,request):
         rooms = Room.objects.all()
-        context = {'rooms':rooms}
+        context = {'rooms':rooms,
+                   'error':self.error}
         reservation = Reservation()
         return render(request,'room/reservation_form.html',context)
     
     def post(self,request):
+        dates_list = []
         room_id = request.POST.get('room')
-        date = datetime.date(datetime.strptime(request.POST.get('date'),'%Y-%m-%d'))
+        rooms = Room.objects.all()
+        try:
+            date = datetime.date(datetime.strptime(request.POST.get('date'),'%Y-%m-%d'))
+        except ValueError:
+            self.error = 'Invalid date format YYYY-MM-DD'
+            context = {'rooms':rooms,
+                       'error':self.error}
+            return render(request,'room/reservation_form.html',context)
+            
         comment = request.POST.get('comment')
         room = Room.objects.get(pk=room_id)
-        reservation = Reservation.objects.create(room_id=room, date=date,comment=comment)
-        return redirect('/')
+        reservations = Reservation.objects.filter(room_id=room_id)
+        for i in reservations:
+            dates_list.append(i.date)
+        
+        if date in dates_list:
+            self.error ='This room is already reserved for this day.'
+            context = {'rooms':rooms,
+                       'error':self.error}
+            return render(request,'room/reservation_form.html',context)
+        elif date < datetime.date(datetime.today()):
+            self.error = 'You cannot reserve room for past date.'
+            context = {'rooms':rooms,
+                       'error':self.error}
+            return render(request,'room/reservation_form.html',context)
+        else:
+            reservation = Reservation.objects.create(room_id=room, date=date,comment=comment)
+            return redirect('/')
     
